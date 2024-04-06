c5ae2f763b4401ecad33eb8d2c0e6531fa262344
Ivan Cheprasov <icheprasov@emphasoft.com>
2024-04-06 21:59:32 +0300

    feat: added regular and persistent data endpoints.

--- shared/src/api/api.ts
+++ shared/src/api/api.ts
@@ -1,12 +1,20 @@
 import { Image } from "../types/image";
 import { Reservation } from "../types/reservation";
 import { User, UserFormValues } from "../types/user";
-import { GENERATED_IMAGES } from "../utils/generate-images";
-import { GENERATED_RESERVATIONS } from "../utils/generate-reservations";
-import { GENERATED_USERS } from "../utils/generate-users";
+import { GENERATED_IMAGES, generateImages } from "../utils/generate-images";
+import { GENERATED_RESERVATIONS, generateReservations } from "../utils/generate-reservations";
+import { GENERATED_USERS, generateUsers } from "../utils/generate-users";
 
 abstract class Api {
   public static async getReservations(): Promise<Reservation[]> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(generateReservations());
+      }, 1000);
+    });
+  }
+
+  public static async getPersistentReservations(): Promise<Reservation[]> {
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve(GENERATED_RESERVATIONS);
@@ -17,12 +25,28 @@ abstract class Api {
   public static async getUsers(): Promise<User[]> {
     return new Promise((resolve) => {
       setTimeout(() => {
+        resolve(generateUsers());
+      }, 1000);
+    });
+  }
+
+  public static async getPersistentUsers(): Promise<User[]> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
         resolve(GENERATED_USERS);
       }, 1000);
     });
   }
 
   public static async getImages(): Promise<Image[]> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(generateImages());
+      }, 1000);
+    });
+  }
+
+  public static async getPersistentImages(): Promise<Image[]> {
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve(GENERATED_IMAGES);
